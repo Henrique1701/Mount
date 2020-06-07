af7f10be9c6bc6b365721b78550be0bdbae19ab4
José Henrique Fernandes Silva <henrique@MacBook-Pro-de-Jose.local>
2020-06-07 12:59:42 -0300

    Adicionando protótipo do projeto

---
+++ Mount/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Mount
+//
+//  Created by José Henrique Fernandes Silva on 03/06/20.
+//  Copyright © 2020 José Henrique Fernandes Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func TocouAtividade1(_ sender: UIButton) {
+        /*
+        let vc = storyboard?.instantiateViewController(identifier: "second_vc") as! SecondViewController
+        present(vc, animated: true)
+         */
+    }
+    
+}
+
---
+++ SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Mount
+//
+//  Created by José Henrique Fernandes Silva on 03/06/20.
+//  Copyright © 2020 José Henrique Fernandes Silva. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
